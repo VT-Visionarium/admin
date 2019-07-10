5c4d33d876c7b204243fd642351dde519817429f
Lance Arsenault <lance@glyph.sv.vt.edu>
2019-07-10 17:47:23 -0400

    save

---
+++ ldap_client/root_install_1.bash
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+function Fail()
+{
+    echo -e "\n$*\n"
+    exit 1
+}
+
+# run as root or not at all
+[ "$(id -u)" = 0 ] || Fail "You must run this as root"
+
+set -xe
+
+scriptdir="$(dirname ${BASH_SOURCE[0]})"
+cd "$scriptdir"
+scriptdir="$PWD"
+
+# at this point we have just updated with apt-get dist-upgrade
+export DEBIAN_FRONTEND=noninteractive
+# cross your fingers that the distro has not broken this yet.
+
+set +e
+
+# It may be that these are not running yet, and may not be
+# installed either, so failing here is fine.
+service nscd stop
+service nslcd stop
+
+
+# ubuntu bug work-around
+# The startup of nslcd gave this error:
+# nslcd: Warning: /lib/x86_64-linux-gnu/libnss_ldap.so.2: undefined symbol: _nss_ldap_enablelookups (probably older NSS module loaded)
+# Remove broken libnss_ldap.so; or not, if it's not there.
+# Why? Because this worked.  Installing a new version of nslcd does not
+# seem to remove this broken version of it.
+apt-get purge libnss-ldapd
+# Why did purge not remove this?
+rm -f /lib/x86_64-linux-gnu/libnss_ldap*
+
+set +x
+echo
+echo "It's okay if some of the last 5 commands failed."
+echo
+set -xe
+
+# This will install:
+# nslcd ldap-utils libnss-ldapd libpam-ldapd nscd
+apt-get -y install nslcd libnss-ldapd
+
+cp ldap.conf /etc/ldap/ldap.conf
+chmod 644 /etc/ldap/ldap.conf
+
+
+set +x
+
+cat << EOF
+
+
+  Okay now run some lib Open LDAP tests like:
+
+
+  ldapsearch -H ldap://login.directory.vt.edu\
+ -x -Z -b ou=People,dc=vt,dc=edu '(uupid=fabidi89)'
+
+
+  Not the ssh tests yet, that's later.
+  And then continue by running 
+  
+      sudo ./root_install_2.bash
+
+EOF
+
---
+++ ldap_client/root_install_2.bash
@@ -0,0 +1,142 @@
+#!/bin/bash
+
+function Fail()
+{
+    echo -e "\n$*\n"
+    exit 1
+}
+
+# run as root or not at all
+[ "$(id -u)" = 0 ] || Fail "You must run this as root"
+
+set -e
+
+scriptdir="$(dirname ${BASH_SOURCE[0]})"
+cd "$scriptdir"
+scriptdir="$PWD"
+
+function Prompt()
+{
+    local a
+    set +x
+    echo
+    echo "    < Cntl-C > to quit"
+    if [ -n "$1" ] ; then
+        echo -ne "\n\n$*\n ===< enter to continue > ===>>>"
+    else
+        echo -ne "\n\n ===< enter to continue > ===>>>"
+    fi
+    read a
+}
+
+
+Prompt "Note this script (2) is interactive, it steps through things"
+
+set -x
+
+if [ ! -f vt-cachain.pem ] ; then
+    # wget the server's public key thingy with:
+    wget --no-check-certificate\
+ http://www.middleware.vt.edu/pubs/vt-cachain.pem
+fi
+
+cp vt-cachain.pem /etc/ca-certificates/vt-cachain.pem
+chmod 644 /etc/ca-certificates/vt-cachain.pem
+
+cp nsswitch.conf /etc/nsswitch.conf
+chmod 644 /etc/nsswitch.conf
+
+cp nslcd.conf /etc/nslcd.conf
+chmod 640 /etc/nslcd.conf
+
+cp adduser.conf /etc/adduser.conf
+chmod 640 /etc/adduser.conf
+
+
+date="$(date)"
+
+######################## edit /etc/pam.d/common-session ##########################
+
+set +x
+Prompt "NEXT with add/check with vim pam_mkhomedir.so (bottom) to /etc/pam.d/common-session"
+
+cat << EOF >> /etc/pam.d/common-session
+
+# lance added next line $date
+session required    pam_mkhomedir.so  skel=/etc/skel  umask=0022
+EOF
+
+vim /etc/pam.d/common-session
+
+set +x
+echo "/etc/pam.d/common-session has been changed"
+echo
+
+######################## edit /etc/pam.d/common-auth ##########################
+
+set +x
+Prompt "NEXT with add/check with vim adding pam_group.so to /etc/pam.d/common-auth"
+
+set -x
+tmp=$(mktemp --suffix=ldap_client_pam_common_)
+cat << EOF > $tmp
+# next line added by lance on $date
+auth    required    pam_group.so use_first_pass
+
+EOF
+cat /etc/pam.d/common-auth >> $tmp
+vim $tmp
+
+Prompt "SAVE this edit as /etc/pam.d/common-auth"
+set -x
+mv $tmp /etc/pam.d/common-auth
+chmod 644 /etc/pam.d/common-auth
+
+####################### change /etc/security/group.conf" ######################
+
+set +x
+Prompt "replace /etc/security/group.conf"
+set -x
+cp group.conf /etc/security/group.conf
+chmod 644 /etc/security/group.conf
+
+
+######################## edit /etc/login.defs ##########################
+
+set +x
+Prompt "NEXT we vim edit /etc/login.defs to change the default UMASK to 022"
+
+set -x
+vim +152 /etc/login.defs
+
+
+
+
+
+set +x
+Prompt "NEXT this script will Restart nscd, and nslcd"
+
+set -x
+service nscd restart
+service nslcd restart
+
+set +x
+
+
+cat << EOF
+
+  Now run some ssh tests with a user that have an LDAP account
+  both with and without a local home directory.
+
+  start and stop nscd and nslcd via 'service nscd start' and so on.
+
+  if it all works leave the services nscd, and nslcd running
+
+      try:
+
+          id ambrown7
+
+      that's it...
+
+EOF
+
