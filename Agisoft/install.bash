9f7648409eef32e7f1dc9808537f4095b11a5155
Lance Arsenault (cube) <lanceman@vt.edu>
2019-10-15 11:19:16 -0400

    added Agisoft metashape installation notes

---
+++ Agisoft/install.bash
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -exuo pipefail
+
+PREFIX=/usr/local/encap/metashape-pro_1_5_5
+
+cd "$(dirname ${BASH_SOURCE[0]})"
+
+dir="$PWD"
+
+make metashape_MovePointer
+
+tarfile=$PWD/metashape-pro_1_5_5_amd64.tar.gz
+
+mkdir -p $PREFIX
+tar --directory=$PREFIX --strip-components=1 -xzf $tarfile
+
+cd $PREFIX
+ls -1 > $PREFIX/encap.exclude
+
+
+mkdir -p $PREFIX/bin
+
+cd $dir
+cp metashape $PREFIX/bin/
+chmod 755 $PREFIX/bin/metashape
+cp metashape_MovePointer metashape_startx $PREFIX/bin
