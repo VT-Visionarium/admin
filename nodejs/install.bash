77b7e9482426f146ded32e35e1946d0394914160
Lance Arsenault <lance.arsenault@gmail.com>
2020-01-13 21:02:05 -0500

    Saving Work

---
+++ nodejs/install.bash
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# This script installs nodeJS from a tagged github.com repository.
+
+set -exo pipefail
+
+cd "$(dirname ${BASH_SOURCE[0]})"
+
+make_opts="-j 30"
+
+tag=v13.6.0
+
+
+tarname=nodejs-$tag
+
+url=https://github.com/nodejs/node/tarball/$tag
+
+prefix=/usr/local/encap/$tarname
+
+tarfile=$tarname.tar.gz
+
+sha512sum=
+
+case "$tag" in
+    v13.6.0)
+        sha512sum=f3142be0a426d4bdc62fd3689841c\
+745fe4df49abf2a3225805e155dac5230fb8625eaa1631d\
+36f7b575e0b9ffb63cdea4482c2ada0332adf7a05e70850\
+e10bc
+        ;;
+esac
+
+
+if [ ! -e "$tarfile" ] ; then
+    wget $url -O $tarfile
+fi
+
+if [ -n "$sha512sum" ] ; then
+    echo "$sha512sum  $tarfile" | sha512sum -C -
+else
+    set +x
+    sha512sum $tarfile
+    set -x
+fi
+
+
+if [ ! -d "$tarname" ] ; then
+    mkdir $tarname
+    cd $tarname
+    tar --strip-components=1 -xzf ../$tarfile
+    cd -
+fi
+
+cd $tarname
+./configure --prefix=$prefix
+make $make_opts
+make install
+
+set +x
+echo "SUCCESSFULLY installed $tarname in $prefix"
